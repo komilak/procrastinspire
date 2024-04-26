82cb85eca49edad9158f6351375e5d0e3175328b
Komila K <2komilak@gmail.com>
2024-04-25 23:31:03 -0700

    intent buttons setup

---
+++ code/Procrastinspire/Procrastinspire/IntentViewController.swift
@@ -0,0 +1,50 @@
+//
+//  IntentViewController.swift
+//  Procrastinspire
+//
+//  Created by Komila Khamidova on 4/25/24.
+//
+
+import UIKit
+
+class IntentViewController: UIViewController {
+
+    
+    @IBOutlet weak var workButton: UIButton!
+    @IBOutlet weak var relaxButton: UIButton!
+    @IBOutlet weak var learnButton: UIButton!
+    @IBOutlet weak var cookButton: UIButton!
+    
+    
+    @IBAction func didTapWorkButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func didTapRelaxButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func didTapLearnButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func didTapCookButton(_ sender: UIButton) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
